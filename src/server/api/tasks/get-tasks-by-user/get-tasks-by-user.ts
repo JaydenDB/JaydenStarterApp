b61f1003fa290fb2a284a4a698c4f271d4235ffb
Jayden Brown <jayden19@byu.edu>
2026-02-02 19:10:06 +0000

    Midway through phase 5

---
+++ src/server/api/tasks/get-tasks-by-user/get-tasks-by-user.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod';
+import { authorizedProcedure } from '../../trpc';
+import { prisma, TaskStatus } from '../../../../../prisma/server';
+
+const getTasksByUserInput = z.object({
+  pageSize: z.number(),
+  pageOffset: z.number(),
+});
+
+const getTasksByUserOutput = z.object({
+  data: z.array(
+    z.object({
+      id: z.string(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+      title: z.string(),
+      description: z.string(),
+      completedDate: z.date().nullable(),
+      userId: z.string(),
+      status: z.literal(Object.values(TaskStatus)),
+    })
+  ),
+  totalCount: z.number(),
+});
+
+export const getTasksByUser = authorizedProcedure
+  .meta({ requiredPermissions: ['manage-tasks'] })
+  .input(getTasksByUserInput)
+  .output(getTasksByUserOutput)
+  .mutation(async opts => {
+    const { pageSize, pageOffset } = opts.input;
+
+    const totalCount = await prisma.task.count({
+      where: {
+        userId: opts.ctx.userId,
+      },
+    });
+
+    const data = await prisma.task.findMany({
+      where: {
+        userId: opts.ctx.userId,
+      },
+      orderBy: { createdAt: 'desc' },
+      skip: pageOffset,
+      take: pageSize,
+    });
+    return { data, totalCount };
+  });
+
+export const createTasksByUser = authorizedProcedure
+  .meta({ requiredPermissions: ['manage-tasks'] })
+  .input(getTasksByUserInput)
+  .output(getTasksByUserOutput)
+  .mutation(async opts => {});
