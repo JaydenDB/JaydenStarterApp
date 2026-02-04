f3697b43a6fae171b8628add5067c30c1549e6d9
Jayden Brown <jayden19@byu.edu>
2026-02-04 23:40:36 +0000

    Jst started Phase 8

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -3,10 +3,11 @@ import { trpcResource } from '@fhss-web-team/frontend-utils';
 import { TRPC_CLIENT } from '../../utils/trpc.client';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { TaskCardComponent } from './task-card/task-card.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [MatProgressSpinnerModule, MatPaginator],
+  imports: [MatProgressSpinnerModule, MatPaginator, TaskCardComponent],
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss',
 })
